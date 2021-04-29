d4ec2abdaf89886382e655b880c04105969d5dd2
Jubair Alam Ovi <jubair.ovi@northsouth.edu>
2021-04-30 03:16:41 +0600

    update mainActivity

--- Theory/Assignment/Assignment03/app/src/main/java/com/example/nsucpc_3/MainActivity.java
+++ Theory/Assignment/Assignment03/app/src/main/java/com/example/nsucpc_3/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.nsucpc_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText emailET,passwordET,birthdayET,phoneET,nidET,presentAddET,permanentAddET,
+            nameET,idET,deptET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        nameET=findViewById(R.id.nameinput);
+        emailET=findViewById(R.id.emailinputET);
+        idET=findViewById(R.id.idinputET);
+        passwordET=findViewById(R.id.passwoedinput);
+        /*btn=findViewById(R.id.login_Button);*/
+        deptET=findViewById(R.id.dept_name_imput);
+        birthdayET=findViewById(R.id.birthday_input);
+        phoneET=findViewById(R.id.phone_input);
+        nidET=findViewById(R.id.nid_input);
+        presentAddET=findViewById(R.id.present_address_input);
+        permanentAddET=findViewById(R.id.permanent_address_input);
     }
 }
