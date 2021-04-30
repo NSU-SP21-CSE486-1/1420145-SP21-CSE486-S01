3e8d219559610fddbaef3f0c5e6b1b1247bdead1
Jubair Alam Ovi <jubair.ovi@northsouth.edu>
2021-05-01 03:37:43 +0600

    Student class

---
+++ Theory/Assignment/Assignment03/app/src/main/java/com/example/nsucpc_3/Student.java
@@ -0,0 +1,16 @@
+package com.example.nsucpc_3;
+
+public class Student {
+
+    private long intial_id;
+    private String name;
+    private String email;
+    private String pass;
+    private String id;
+    private String dept;
+    private String birthday;
+    private String nid;
+    private String phone;
+    private String presentAddress;
+    private String permanentAddress;
+}
