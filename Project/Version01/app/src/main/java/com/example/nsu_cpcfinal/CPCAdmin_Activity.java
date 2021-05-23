77bac815fb453bb281e03d468650332b2fc7d81d
Oviex <jubair.ovi@northsouth.edu>
2021-05-23 20:47:43 +0600

    CPC Admin Activity
    Login ,Reg and cpc admin activity

---
+++ Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/CPCAdmin_Activity.java
@@ -0,0 +1,14 @@
+package com.example.nsu_cpcfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CPCAdmin_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cpcadmin);
+    }
+}
