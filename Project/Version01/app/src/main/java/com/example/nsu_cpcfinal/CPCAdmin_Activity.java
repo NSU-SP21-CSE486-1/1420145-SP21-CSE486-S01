3cf2c3151a0a57f92a95d661c50187f8d8aa46f2
Oviex <jubair.ovi@northsouth.edu>
2021-05-26 03:31:03 +0600

    Firebase connection solved

--- Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/CPCAdmin_Activity.java
+++ Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/CPCAdmin_Activity.java
@@ -10,5 +10,7 @@ public class CPCAdmin_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cpcadmin);
+
+        getSupportActionBar().hide();
     }
 }
--- Theory/Assignment/Assignment03/app/src/main/java/com/example/nsucpc_3/MainActivity.java
+++ Theory/Assignment/Assignment03/app/src/main/java/com/example/nsucpc_3/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.widget.EditText;
 
