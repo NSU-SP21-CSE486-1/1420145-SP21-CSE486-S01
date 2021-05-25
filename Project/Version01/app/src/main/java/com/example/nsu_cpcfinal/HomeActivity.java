e2cb4d62b5f7de56e84a0777be9619da6d5303fd
Jubair Alam Ovi <jubair.ovi@northsouth.edu>
2021-05-25 16:09:28 +0600

    Home Activity to Registration Activity

--- Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/HomeActivity.java
+++ Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/HomeActivity.java
@@ -2,9 +2,14 @@ package com.example.nsu_cpcfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button signup_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,13 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         getSupportActionBar().hide();
+
+        signup_btn=findViewById(R.id.sign_up);
+    }
+
+    public void openRegActivity(View view) {
+
+        Intent intent =new Intent(HomeActivity.this,RegistrationActivity.class);
+        startActivity(intent);
     }
 }
