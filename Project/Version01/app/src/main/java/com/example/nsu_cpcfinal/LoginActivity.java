981eeb16c2875def8b742211a4b1f27bf9e16e91
Oviex <jubair.ovi@northsouth.edu>
2021-05-26 16:15:05 +0600

    Firebase Auth

--- Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/LoginActivity.java
+++ Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/LoginActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText nameLog, emailLog, deptLog;
+    private EditText  emailLog,passLog ;
     private Button login_btn_log;
 
     @Override
@@ -21,31 +21,33 @@ public class LoginActivity extends AppCompatActivity {
 
     private void loginFunction(){
 
-        nameLog =findViewById(R.id.add_nameInput);
+
         emailLog = findViewById(R.id.add_email_Input);
-        deptLog =findViewById(R.id.add_dept_Input);
+        passLog = findViewById(R.id.add_pass_Input);
+        /*deptLog =findViewById(R.id.add_dept_Input);*/
         login_btn_log =findViewById(R.id.login);
 
         login_btn_log.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name =nameLog.getText().toString().trim();
+
                 String email =emailLog.getText().toString().trim();
-                String dept = deptLog.getText().toString().trim();
+                String pass =passLog.getText().toString().trim();
+                /*String dept = deptLog.getText().toString().trim();*/
 
 
-                if (TextUtils.isEmpty(name)){
-                    nameLog.setError("Required field");
+                if (TextUtils.isEmpty(pass)){
+                    passLog.setError("Required field");
 
                 }
                 if (TextUtils.isEmpty(email)){
                     emailLog.setError("Required field");
 
                 }
-                if (TextUtils.isEmpty(dept)){
+                /*if (TextUtils.isEmpty(dept)){
                     deptLog.setError("Required field");
 
-                }
+                }*/
 
             }
         });
--- Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/RegistrationActivity.java
+++ Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/RegistrationActivity.java
@@ -1,14 +1,21 @@
 package com.example.nsu_cpcfinal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -16,9 +23,12 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    private EditText nameReg, emailReg, deptReg,birthdayReg,
-            nidReg,phnReg, presentReg,permanentReg;
+    private EditText  emailReg,passReg/*, deptReg,birthdayReg,
+            nidReg,phnReg, presentReg,permanentReg*/;
     private Button reg_btnReg;
+
+    FirebaseAuth mAuth;
+
 
     /*private DatabaseReference rootRef;
     private DatabaseReference userRef;
@@ -36,6 +46,7 @@ public class RegistrationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
 
         regFunction();
+        mAuth=FirebaseAuth.getInstance();
 
         /*private EditText nameReg, emailReg, deptReg,birthdayReg,
                 nidReg,phnReg, presentReg,permanentReg;*/
@@ -48,48 +59,45 @@ public class RegistrationActivity extends AppCompatActivity {
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
         userIdRef = userRef.child(currentUser.getUid());
         studentRef = userIdRef.child("Students");*/
-
-
-
-
-
-
-
     }
 
     private void regFunction(){
 
-        nameReg=findViewById(R.id.add_reg_NameInput);
+
         emailReg=findViewById(R.id.add_reg_email_Input);
-        deptReg= findViewById(R.id.add_reg_dept_Input);
+        passReg=findViewById(R.id.add_reg_passInput);
+        reg_btnReg=findViewById(R.id.add_reg_registration_Input);
+        /*deptReg= findViewById(R.id.add_reg_dept_Input);
         birthdayReg=findViewById(R.id.add_reg_birthday_Input);
         nidReg=findViewById(R.id.add_reg_nid_Input);
         phnReg=findViewById(R.id.add_reg_phone_Input);
         presentReg=findViewById(R.id.add_reg_present_Input);
         permanentReg=findViewById(R.id.add_reg_permanent_Input);
-        reg_btnReg=findViewById(R.id.add_reg_registration_Input);
+        reg_btnReg=findViewById(R.id.add_reg_registration_Input);*/
 
         reg_btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String nameR=nameReg.getText().toString().trim();
+
                 String emailR=emailReg.getText().toString().trim();
-                String deptR=deptReg.getText().toString().trim();
+                String passR=passReg.getText().toString().trim();
+                /*String deptR=deptReg.getText().toString().trim();
                 String birthdayR=birthdayReg.getText().toString().trim();
                 String nidR=nidReg.getText().toString().trim();
                 String phoneR=phnReg.getText().toString().trim();
                 String presentR=presentReg.getText().toString().trim();
-                String permanentR=permanentReg.getText().toString().trim();
+                String permanentR=permanentReg.getText().toString().trim();*/
 
-                if (TextUtils.isEmpty(nameR)){
-                    nameReg.setError("Required field");
 
-                }
                 if (TextUtils.isEmpty(emailR)){
                     emailReg.setError("Required field");
 
                 }
-                if (TextUtils.isEmpty(deptR)){
+                if (TextUtils.isEmpty(passR)){
+                    passReg.setError("Required field");
+
+                }
+                /*if (TextUtils.isEmpty(deptR)){
                     deptReg.setError("Required field");
 
                 }
@@ -113,6 +121,29 @@ public class RegistrationActivity extends AppCompatActivity {
                     permanentReg.setError("Required field");
 
                 }
+
+*/
+                mAuth.createUserWithEmailAndPassword(emailR,passR)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                Toast.makeText(RegistrationActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(),JobActivity.class));
+
+                            }
+
+
+                        })
+
+                        .addOnFailureListener(new OnFailureListener() {
+
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(RegistrationActivity.this, "Unsuccessful", Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
 
 
 
