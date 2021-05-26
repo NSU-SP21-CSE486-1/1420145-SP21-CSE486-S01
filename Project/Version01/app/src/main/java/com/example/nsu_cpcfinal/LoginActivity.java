ef083540cf6208fb1883d7deb25288d07cf54825
Oviex <jubair.ovi@northsouth.edu>
2021-05-26 15:09:00 +0600

    Update registration and login

--- Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/LoginActivity.java
+++ Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/LoginActivity.java
@@ -3,12 +3,52 @@ package com.example.nsu_cpcfinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText nameLog, emailLog, deptLog;
+    private Button login_btn_log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginFunction();
+    }
+
+    private void loginFunction(){
+
+        nameLog =findViewById(R.id.add_nameInput);
+        emailLog = findViewById(R.id.add_email_Input);
+        deptLog =findViewById(R.id.add_dept_Input);
+        login_btn_log =findViewById(R.id.login);
+
+        login_btn_log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name =nameLog.getText().toString().trim();
+                String email =emailLog.getText().toString().trim();
+                String dept = deptLog.getText().toString().trim();
+
+
+                if (TextUtils.isEmpty(name)){
+                    nameLog.setError("Required field");
+
+                }
+                if (TextUtils.isEmpty(email)){
+                    emailLog.setError("Required field");
+
+                }
+                if (TextUtils.isEmpty(dept)){
+                    deptLog.setError("Required field");
+
+                }
+
+            }
+        });
+
     }
 }
--- Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/RegistrationActivity.java
+++ Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/RegistrationActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -13,16 +15,16 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrationActivity extends AppCompatActivity {
-    private Context context;
-    private EditText nameET, emailET,idlET, deptET,birthdayET,
-            nidET,phnET, presentET,permanentT;
-    private Button addBtn;
 
-    private DatabaseReference rootRef;
+    private EditText nameReg, emailReg, deptReg,birthdayReg,
+            nidReg,phnReg, presentReg,permanentReg;
+    private Button reg_btnReg;
+
+    /*private DatabaseReference rootRef;
     private DatabaseReference userRef;
     private DatabaseReference userIdRef;
     private DatabaseReference studentRef;
-    private FirebaseUser currentUser;
+    private FirebaseUser currentUser;*/
     /*private OnAddStudentSuccessListener listener;*/
 
 
@@ -33,27 +35,90 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        nameET = findViewById(R.id.add_empNameInput);
-        emailET = findViewById(R.id.add_email_Input);
-        /*idlET = view.findViewById(R.id.add_id_Input);*/
-        deptET = findViewById(R.id.add_dept_Input);
-        birthdayET = findViewById(R.id.add_birthday_Input);
-        nidET = findViewById(R.id.add_nid_Input);
-        phnET = findViewById(R.id.add_phone_Input);
-        presentET = findViewById(R.id.add_present_Input);
-        permanentT = findViewById(R.id.add_permanent_Input);
-        addBtn = findViewById(R.id.addNewEmployee);
+        regFunction();
 
-        rootRef = FirebaseDatabase.getInstance().getReference();
+        /*private EditText nameReg, emailReg, deptReg,birthdayReg,
+                nidReg,phnReg, presentReg,permanentReg;*/
+
+
+
+
+        /*rootRef = FirebaseDatabase.getInstance().getReference();
         userRef = rootRef.child("Users");
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
         userIdRef = userRef.child(currentUser.getUid());
-        studentRef = userIdRef.child("Students");
+        studentRef = userIdRef.child("Students");*/
 
 
 
 
 
+
+
+    }
+
+    private void regFunction(){
+
+        nameReg=findViewById(R.id.add_reg_NameInput);
+        emailReg=findViewById(R.id.add_reg_email_Input);
+        deptReg= findViewById(R.id.add_reg_dept_Input);
+        birthdayReg=findViewById(R.id.add_reg_birthday_Input);
+        nidReg=findViewById(R.id.add_reg_nid_Input);
+        phnReg=findViewById(R.id.add_reg_phone_Input);
+        presentReg=findViewById(R.id.add_reg_present_Input);
+        permanentReg=findViewById(R.id.add_reg_permanent_Input);
+        reg_btnReg=findViewById(R.id.add_reg_registration_Input);
+
+        reg_btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nameR=nameReg.getText().toString().trim();
+                String emailR=emailReg.getText().toString().trim();
+                String deptR=deptReg.getText().toString().trim();
+                String birthdayR=birthdayReg.getText().toString().trim();
+                String nidR=nidReg.getText().toString().trim();
+                String phoneR=phnReg.getText().toString().trim();
+                String presentR=presentReg.getText().toString().trim();
+                String permanentR=permanentReg.getText().toString().trim();
+
+                if (TextUtils.isEmpty(nameR)){
+                    nameReg.setError("Required field");
+
+                }
+                if (TextUtils.isEmpty(emailR)){
+                    emailReg.setError("Required field");
+
+                }
+                if (TextUtils.isEmpty(deptR)){
+                    deptReg.setError("Required field");
+
+                }
+                if (TextUtils.isEmpty(birthdayR)){
+                    birthdayReg.setError("Required field");
+
+                }
+                if (TextUtils.isEmpty(nidR)){
+                    nidReg.setError("Required field");
+
+                }
+                if (TextUtils.isEmpty(phoneR)){
+                    phnReg.setError("Required field");
+
+                }
+                if (TextUtils.isEmpty(presentR)){
+                    presentReg.setError("Required field");
+
+                }
+                if (TextUtils.isEmpty(permanentR)){
+                    permanentReg.setError("Required field");
+
+                }
+
+
+
+
+            }
+        });
 
 
     }
