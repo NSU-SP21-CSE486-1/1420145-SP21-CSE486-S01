e19baf010a3f6570240dda0249187538fe5897e5
Oviex <jubair.ovi@northsouth.edu>
2021-05-26 16:51:46 +0600

    Update Login Activity

--- Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/LoginActivity.java
+++ Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/LoginActivity.java
@@ -1,21 +1,33 @@
 package com.example.nsu_cpcfinal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText  emailLog,passLog ;
     private Button login_btn_log;
 
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        firebaseAuth=FirebaseAuth.getInstance();
         loginFunction();
     }
 
@@ -49,8 +61,35 @@ public class LoginActivity extends AppCompatActivity {
 
                 }*/
 
+                firebaseAuth.signInWithEmailAndPassword(email,pass)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                Toast.makeText(LoginActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(),JobActivity.class));
+
+                            }
+
+
+                        })
+
+                        .addOnFailureListener(new OnFailureListener() {
+
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(LoginActivity.this, "Unsuccessful", Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+
+
+
+
             }
         });
+
+            
 
     }
 }
