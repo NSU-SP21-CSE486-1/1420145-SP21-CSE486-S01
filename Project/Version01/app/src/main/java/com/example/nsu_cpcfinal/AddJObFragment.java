fdf7c2746027db8172a7330e57eb7bc3dd26e2d2
Oviex <jubair.ovi@northsouth.edu>
2021-05-27 00:04:34 +0600

    Fragment issue

--- Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/AddJObFragment.java
+++ Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/AddJObFragment.java
@@ -1,30 +1,51 @@
 package com.example.nsu_cpcfinal;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.jetbrains.annotations.NotNull;
 
 
 public class AddJObFragment extends Fragment {
 
 
+    private Context context;
+
+    private EditText nameJob,deptJob,descriptionJOb,salaryJob,statusJOb;
+    private Button addJobBtn;
+    private Addjoblistner addjoblistner;
+
     public AddJObFragment() {
         // Required empty public constructor
     }
+
+    @Override
+    public void onAttach(@NonNull @org.jetbrains.annotations.NotNull Context context) {
+        super.onAttach(context);
+        this.context=context;
+        //jobListListner= (JobPortalFragment.jobListListner) context;
+        addjoblistner= (Addjoblistner) context;
+    }
+
 
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
+
 
         }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,4 +53,28 @@ public class AddJObFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_j_ob, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        nameJob=view.findViewById(R.id.add_Job_NameInput);
+        deptJob=view.findViewById(R.id.add_job_dept_Input);
+        descriptionJOb=view.findViewById(R.id.add_job_description_Input);
+        salaryJob=view.findViewById(R.id.add_job_salary_Input);
+        statusJOb=view.findViewById(R.id.add_job_status_Input);
+        addJobBtn=view.findViewById(R.id.job_addButton);
+        addJobBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addjoblistner.onAddJobButtonClicl();
+
+            }
+        });
+    }
+    public interface Addjoblistner{
+        void onAddJobButtonClicl();
+    }
+
+
+
 }
--- Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/CpcLoginActivity.java
+++ Project/Version01/app/src/main/java/com/example/nsu_cpcfinal/CpcLoginActivity.java
@@ -2,6 +2,8 @@ package com.example.nsu_cpcfinal;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,26 +19,34 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class CpcLoginActivity extends AppCompatActivity {
+public class CpcLoginActivity extends AppCompatActivity  {
 
     private EditText cpc_emailLog,cpc_passLog;
     private Button cpc_btnLog;
     private FirebaseAuth firebaseAuth;
 
+    private FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cpc_login);
-        firebaseAuth=FirebaseAuth.getInstance();
+        /*firebaseAuth=FirebaseAuth.getInstance();
 
-        cpcLoginFunction();
+        cpcLoginFunction();*/
+
+        fragmentManager=getSupportFragmentManager();
+        FragmentTransaction ft=fragmentManager.beginTransaction();
+        LoginCpcFragment loginCpcFragment=new LoginCpcFragment();
+        ft.add(R.id.fragmentContainer,loginCpcFragment);
+        ft.commit();
     }
 
-    private void cpcLoginFunction(){
+    /*private void cpcLoginFunction(){
 
         cpc_emailLog = findViewById(R.id.add_cpclogin_email_Input);
         cpc_passLog= findViewById(R.id.add_cpclogin_pass_Input);
-        /*deptLog =findViewById(R.id.add_dept_Input);*/
+        *//*deptLog =findViewById(R.id.add_dept_Input);*//*
         cpc_btnLog =findViewById(R.id.cpc_login);
 
         cpc_btnLog.setOnClickListener(new View.OnClickListener() {
@@ -45,7 +55,7 @@ public class CpcLoginActivity extends AppCompatActivity {
 
                 String email =cpc_emailLog.getText().toString().trim();
                 String pass =cpc_passLog.getText().toString().trim();
-                /*String dept = deptLog.getText().toString().trim();*/
+                *//*String dept = deptLog.getText().toString().trim();*//*
 
 
                 if (TextUtils.isEmpty(email)){
@@ -56,10 +66,10 @@ public class CpcLoginActivity extends AppCompatActivity {
                     cpc_passLog.setError("Required field");
 
                 }
-                /*if (TextUtils.isEmpty(dept)){
+                *//*if (TextUtils.isEmpty(dept)){
                     deptLog.setError("Required field");
 
-                }*/
+                }*//*
 
                 firebaseAuth.signInWithEmailAndPassword(email,pass)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -90,7 +100,6 @@ public class CpcLoginActivity extends AppCompatActivity {
         });
 
 
-
+*/
 
     }
-}
